fe8805395b5f004c7606c8e6aed74870939f4169
h70076333 <h70076222@88.com>
2026-03-14 22:21:18 +0800

    Update web_ex.c

--- trunk/user/httpd/web_ex.c
+++ trunk/user/httpd/web_ex.c
@@ -2065,6 +2065,60 @@ net_iface_list_hook(int eid, webs_t wp, int argc, char **argv)
 static int
 net_update_vpnc_wg_state_hook(int eid, webs_t wp, int argc, char **argv)
 {
+#if defined (APP_ZEROTIER)
+static int zerotier_status_hook(int eid, webs_t wp, int argc, char **argv)
+{
+	int zerotier_status_code = pids("zerotier-one");
+	websWrite(wp, "function zerotier_status() { return %d;}\n", zerotier_status_code);
+	return 0;
+}
+#endif
+
+#if defined (APP_HXCLI)
+static int hxcli_status_hook(int eid, webs_t wp, int argc, char **argv)
+{
+	int hxcli_status_code = pids("hx-cli");
+	websWrite(wp, "function hxcli_status() { return %d;}\n", hxcli_status_code);
+	return 0;
+}
+#endif
+
+#if defined (APP_NELINK)
+static int nelink_status_hook(int eid, webs_t wp, int argc, char **argv)
+{
+	int nelink_status_code = pids("netlink");
+	websWrite(wp, "function nelink_status() { return %d;}\n", nelink_status_code);
+	return 0;
+}
+#endif
+
+#if defined (APP_NTWON)
+static int ntwon_status_hook(int eid, webs_t wp, int argc, char **argv)
+{
+	int ntwon_status_code = pids("ntwon");
+	websWrite(wp, "function ntwon_status() { return %d;}\n", ntwon_status_code);
+	return 0;
+}
+#endif
+
+#if defined (APP_ETINK)
+static int etink_status_hook(int eid, webs_t wp, int argc, char **argv)
+{
+	int etink_status_code = pids("easytier-core");
+	websWrite(wp, "function etink_status() { return %d;}\n", etink_status_code);
+	return 0;
+}
+#endif
+	
+#if defined (APP_BAFA)
+static int bafa_status_hook(int eid, webs_t wp, int argc, char **argv)
+{
+	int bafa_status_code = pids("stdoutsubc") || pids("stdoutsub");
+	websWrite(wp, "function bafa_status() { return %d;}\n", bafa_status_code);
+	return 0;
+}
+#endif
+
 #if defined(APP_WIREGUARD)
     if (nvram_get_int("vpnc_enable") == 0 || nvram_get_int("vpnc_type") != 3) {
         return 0;
@@ -2236,6 +2290,34 @@ ej_firmware_caps_hook(int eid, webs_t wp, int argc, char **argv)
 #else
 	int found_app_vlmcsd = 0;
 #endif
+#if defined(APP_ZEROTIER)
+	int found_app_zerotier = 1;
+#else
+	int found_app_zerotier = 0;
+#endif
+#if defined(APP_HXCLI)
+	int found_app_hxcli = 1;
+#else
+	int found_app_hxcli = 0;
+#endif
+#if defined(APP_NELINK)
+	int found_app_nelink = 1;
+#else
+	int found_app_nelink = 0;
+#endif
+#if defined(APP_NTWON)
+	int found_app_ntwon = 1;
+#else
+	int found_app_ntwon = 0;
+#endif
+#if defined(APP_ETINK)
+	int found_app_etink = 1;
+#else
+	int found_app_etink = 0;
+#endif
+#if defined(APP_BAFA)
+	int found_app_bafa = 1;
+#else
 #if defined(APP_IPERF3)
 	int found_app_iperf3 = 1;
 #else
@@ -2404,6 +2486,12 @@ ej_firmware_caps_hook(int eid, webs_t wp, int argc, char **argv)
 		"function found_support_zram() { return %d;}\n"
 		"function found_app_xupnpd() { return %d;}\n"
 		"function found_app_vlmcsd() { return %d;}\n"
+		"function found_app_zerotier() { return %d;}\n"
+		"function found_app_hxcli() { return %d;}\n"
+		"function found_app_nelink() { return %d;}\n"
+		"function found_app_ntwon() { return %d;}\n"
+		"function found_app_etink() { return %d;}\n"
+		"function found_app_bafa() { return %d;}\n"
 		"function found_app_iperf3() { return %d;}\n",
 		found_utl_hdparm,
 		found_app_ovpn,
@@ -2430,6 +2518,12 @@ ej_firmware_caps_hook(int eid, webs_t wp, int argc, char **argv)
 		found_support_zram,
 		found_app_xupnpd,
 		found_app_vlmcsd,
+		found_app_zerotier,
+		found_app_hxcli,
+		found_app_nelink,
+		found_app_ntwon,
+		found_app_etink,
+		found_app_bafa,
 		found_app_iperf3
 	);
 
@@ -3249,6 +3343,161 @@ apply_cgi(const char *url, webs_t wp)
 		websRedirect(wp, current_url);
 		return 0;
 	}
+	else if (!strcmp(value, " RestartBAFA "))
+	{
+#if defined(APP_BAFA)
+		system("/usr/bin/bafa.sh restart &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " RestartNTWON "))
+	{
+#if defined(APP_NTWON)
+		system("/usr/bin/ntwon.sh restart &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " Restartnelink "))
+	{
+#if defined(APP_NELINK)
+		system("/usr/bin/ne.sh restart &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " Restartetink "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/etink.sh restart &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " Updateeasytier "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh update &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetpeer "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh peer &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetconnector "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh connector &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetstun "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh stun &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetroute "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh route &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetpeer_center "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh peer-center &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetvpn_portal "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh vpn-portal &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetnode "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh node &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetproxy "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/easytier.sh proxy &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDetstatus "))
+	{
+#if defined(APP_ETINK)
+		system("/usr/bin/etink.sh status &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " Restarthxcli "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh restart &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " Updatehxcli "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh update &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDhxinfo "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh hxinfo &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDhxall "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh hxall &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDhxlist "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh hxlist &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDhxroute "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh hxroute &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " CMDhxstatus "))
+	{
+#if defined(APP_HXCLI)
+		system("/usr/bin/hxzn.sh hxstatus &");
+#endif
+		return 0;
+	}
+	else if (!strcmp(value, " ClearhxcliLog "))
+	{
+#if defined(APP_HXCLI)
+		unlink("/tmp/hx-cli.log");
+#endif
+		websRedirect(wp, wg_action);
+		return 0;
+	}
 #if defined(APP_WIREGUARD)
 	else if (!strcmp(value, " wg_action "))
 	{
@@ -3894,6 +4143,64 @@ static char no_cache_IE[] =
 "Pragma: no-cache\r\n"
 "Expires: -1"
 ;
+#if defined (APP_HXCLI)
+static void
+do_hxcli_log_file(const char *url, FILE *stream)
+{
+	dump_file(stream, "/tmp/hx-cli.log");
+	fputs("\r\n", stream);
+}
+
+static char hxcli_log_txt[] =
+"Content-Disposition: attachment;\r\n"
+"filename=hx-cli.log"
+;
+
+#endif
+
+#if defined (APP_NELINK)
+static void
+do_nelink_log_file(const char *url, FILE *stream)
+{
+	dump_file(stream, "/tmp/nelink.log");
+	fputs("\r\n", stream);
+}
+
+static char nelink_log_txt[] =
+"Content-Disposition: attachment;\r\n"
+"filename=nelink.log"
+;
+
+#endif
+#if defined (APP_NTWON)
+static void
+do_ntwon_log_file(const char *url, FILE *stream)
+{
+	dump_file(stream, "/tmp/ntwon.log");
+	fputs("\r\n", stream);
+}
+
+static char ntwon_log_txt[] =
+"Content-Disposition: attachment;\r\n"
+"filename=ntwon.log"
+;
+
+#endif
+
+#if defined (APP_ETINK)
+static void
+do_etink_log_file(const char *url, FILE *stream)
+{
+	dump_file(stream, "/tmp/etink.log");
+	fputs("\r\n", stream);
+}
+
+static char etink_log_txt[] =
+"Content-Disposition: attachment;\r\n"
+"filename=etink.log"
+;
+
+#endif
 
 struct mime_handler mime_handlers[] = {
 	/* cached javascript files w/o translations */
@@ -3937,6 +4244,18 @@ struct mime_handler mime_handlers[] = {
 	{ "syslog.txt", "application/force-download", syslog_txt, NULL, do_syslog_file, 1 },
 #if defined(APP_OPENVPN)
 	{ "client.ovpn", "application/force-download", NULL, NULL, do_export_ovpn_client, 1 },
+#endif
+#if defined(APP_HXCLI)
+	{ "hx-cli.log", "application/force-download", hxcli_log_txt, NULL, do_hxcli_log_file, 1 },
+#endif
+#if defined(APP_NELINK)
+	{ "nelink.log", "application/force-download", nelink_log_txt, NULL, do_nelink_log_file, 1 },
+#endif
+#if defined(APP_NTWON)
+	{ "ntwon.log", "application/force-download", ntwon_log_txt, NULL, do_ntwon_log_file, 1 },
+#endif
+#if defined(APP_ETINK)
+	{ "etink.log", "application/force-download", etink_log_txt, NULL, do_etink_log_file, 1 },
 #endif
 
 	/* no-cached POST objects */
@@ -4232,6 +4551,21 @@ struct ej_handler ej_handlers[] =
 	{ "delete_sharedfolder", ej_delete_sharedfolder},
 	{ "modify_sharedfolder", ej_modify_sharedfolder},
 	{ "set_share_mode", ej_set_share_mode},
+#endif
+#if defined (APP_ZEROTIER)
+	{ "zerotier_status", zerotier_status_hook},
+#endif
+#if defined (APP_HXCLI)
+	{ "hxcli_status", hxcli_status_hook},
+#endif
+#if defined (APP_NELINK)
+	{ "nelink_status", nelink_status_hook},
+#endif
+#if defined (APP_NTWON)
+	{ "ntwon_status", ntwon_status_hook},
+#endif
+#if defined (APP_ETINK)
+	{ "etink_status", etink_status_hook},
 #endif
 	{ "update_action", update_action_hook},
 	{ "openssl_util_hook", openssl_util_hook},
