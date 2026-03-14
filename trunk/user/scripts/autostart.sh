5a3016a8415e33116fb1d08662046692a355105a
h70076333 <h70076222@88.com>
2026-03-14 22:58:50 +0800

    Update autostart.sh

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -20,3 +20,32 @@ do
 		break
 	fi
 done
+
+if [ $(nvram get bafa_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动巴法云物联网"
+/usr/bin/bafa.sh start &
+fi
+if [ $(nvram get hxcli_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动异地组网"
+/usr/bin/hx.sh start &
+fi
+
+if [ $(nvram get nelink_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动NE异地组网"
+/usr/bin/ne.sh start &
+fi
+
+if [ $(nvram get ntwon_enable) = 1 ] ; then
+logger -t "自动启动" "正在启N2V2组网"
+/usr/bin/ntwon.sh start &
+fi
+
+if [ $(nvram get etink_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动ET异地组网"
+/usr/bin/etink.sh start &
+fi
+
+if [ $(nvram get etweb_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动ETWEB异地组网"
+/usr/bin/etink.sh start &
+fi
